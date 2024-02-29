f2a1e184fdb4f541000861bb954e29df3395afb4
Anasta16 <97866619+Anasta16@users.noreply.github.com>
2024-02-29 00:58:27 -0500

    created node server, created react app frontend, added redux and created initial state for username, created routing using deprecated routing techniques

---
+++ test-video-frontend/src/App.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from 'react-router-dom';
+import { connectWithWebSocket } from './utils/wssConnection/wssConnection';
+import Dashboard from './Dashboard/Dashboard';
+import LoginPage from './LoginPage/LoginPage';
+
+function App() {
+
+  useEffect(() => {
+    connectWithWebSocket();
+  }, []);
+
+  return (
+    <Router>
+      <Switch>
+        <Route path="/dashboard" >
+          <Dashboard />
+        </Route>
+        <Route path="/" >
+          <LoginPage />
+        </Route>
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
---
+++ test-video-frontend/src/store/store.js
@@ -0,0 +1,10 @@
+import { legacy_createStore as createStore } from 'redux';
+import mainReducer from './reducer';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+const store = createStore(
+    mainReducer,
+    composeWithDevTools()
+);
+
+export default store;
